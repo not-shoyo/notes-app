a8eb8c77c560786cc4ba57a090b411b8df714bb6
not-shoyo <akheelachu@gmail.com>
2021-06-24 21:31:58 +0300

    center title hint 1

--- lib/addnotespage.dart
+++ lib/addnotespage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/model/note.dart';
 import 'package:flutter_application_2/mynotes.dart';
+import 'package:simple_text_field/simple_text_field.dart';
 
 /*Making an addnotespage where user will write their note
 * once they finish we will set it to the content of that note in notes[i]
@@ -16,17 +17,13 @@ class AddNotesPage extends StatefulWidget {
 
 /// This is the private State class that goes with AddNotesPage.
 class _AddNotesPageState extends State<AddNotesPage> {
-  late TextEditingController _noteController;
-
-  @override
-  void initState() {
-    super.initState();
-    _noteController = TextEditingController();
-  }
+  final TextEditingController _noteController = TextEditingController();
+  final TextEditingController _titleController = TextEditingController();
 
   @override
   void dispose() {
     _noteController.dispose();
+    _titleController.dispose();
     super.dispose();
   }
 
@@ -34,47 +31,46 @@ class _AddNotesPageState extends State<AddNotesPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(
-          tooltip: "Notes Page",
-          icon: const Icon(Icons.notes, color: Colors.white,),
-          onPressed: _backButtonClicked,
-          ),
-        title: IconButton(
+        automaticallyImplyLeading: false,
+        title: ListTile(
+          leading: IconButton(
+            tooltip: "Notes Page",
+            icon: const Icon(Icons.notes, color: Colors.white,),
+            onPressed: _backButtonClicked,
+            ),
+          trailing: IconButton(
             tooltip: "Save",
             icon: const Icon(Icons.save, color: Colors.white),
             onPressed: _saveClicked,
-            )
+            ),
+          title: TextField(
+            decoration: const InputDecoration(                        //making the hint and input come in the middle
+              hintText: "title",
+              hintStyle: TextStyle(
+                color: Colors.white60,
+              ),
+              enabledBorder: InputBorder.none,
+            ),
+            controller: _titleController,
+            cursorColor: Colors.white,
+            style: const TextStyle(color: Colors.white),
+            maxLines: 1,
           ),
-      body: Center(
-        widthFactor: 50.0,
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: TextField(
           maxLines: null,
           minLines: null,
           expands: true,
           controller: _noteController,
-          onSubmitted: (String value) async {
-            await showDialog<void>(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  title: const Text('Enter name of note: '),
-                  content: const TextField(),
-                  actions: <Widget>[
-                    TextButton(
-                      onPressed: _backButtonClicked,
-                      child: const Text('Set Note Name'),
-                    ),
-                  ],
-                );
-              },
-            );
-          },
         ),
       ),
     );
   }
 
-  _saveClicked(){
+  void _saveClicked(){
     if (MyNotes.notes.isNotEmpty){
       int n = MyNotes.notes.length;
       MyNotes.notes.add(Note("note $n"));
@@ -86,7 +82,7 @@ class _AddNotesPageState extends State<AddNotesPage> {
     Navigator.pop(context);
   }
 
-  _backButtonClicked(){
+  void _backButtonClicked(){
     Navigator.pop(context);
   }
 }
